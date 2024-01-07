6d1defa43f6d05b9f4f81e0f5ef50cd6fcbd9303
Mohammad Touhidul Islam <touhidulfahim@hotmail.com>
2024-01-07 13:44:17 +0600

    all separated component in one place

--- NgCoreClient/src/app/app.module.ts
+++ NgCoreClient/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
-import { AddEditDepartmentComponent } from './department/add-edit-department/add-edit-department.component';
-import { ShowDepartmentComponent } from './department/show-department/show-department.component';
 import { StudentComponent } from './student/student.component';
 import { AddEditStudentComponent } from './student/add-edit-student/add-edit-student.component';
 import { ShowStudentComponent } from './student/show-student/show-student.component';
@@ -17,8 +15,6 @@ import { MyModalComponent } from './my-modal/my-modal.component';
 @NgModule({
   declarations: [
     DepartmentComponent,
-    ShowDepartmentComponent,
-    AddEditDepartmentComponent,
     StudentComponent,
     ShowStudentComponent,
     AddEditStudentComponent,    
